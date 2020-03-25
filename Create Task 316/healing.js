e93525d00ccf8ab4cc55bdf41fb73cde6671ccb8
802100 <54591147+802100@users.noreply.github.com>
2020-03-25 09:53:45 -0700

    CreateTask 316
    i fixed up the healing class and added in the isColliding function.

--- Create Task 316/healing.js
+++ Create Task 316/healing.js
@@ -12,8 +12,30 @@ class Healing{
     this.render();
 
   }
+  //if healing ball collides with the paddle return true
+  isColliding(){
+    if(this.loc.x>paddle.loc.x &&
+      this.loc.x<paddle.loc.x + paddle.w &&
+      this.loc.y>paddle.loc.y &&
+      this.loc.y<paddle.loc.y + paddle.h){
+        return true;
+      }else{
+        return false;
+      }
+    }
 
   checkEdges(){
+    for(var i = 0; i < healing.length; i++){
+      if(healing[i].isColliding()){
+        console.log("healing collision");
+        healing.splice(i,1);
+        lives = lives+1;
+      }
+    }
+
+
+
+
     //if hitting edges of the screen
     if(this.loc.x < 0){
       this.vel.x = -this.vel.x;
